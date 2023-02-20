c7253deb847d5dcddbf65b64b26a26265b88510e
newcrossing <newcrossing@gmail.com>
2023-02-20 23:27:03 +0300

    регулярка

--- app/Http/Controllers/Adm/DocController.php
+++ app/Http/Controllers/Adm/DocController.php
@@ -191,9 +191,11 @@ class DocController extends Controller
         $doc->fill($request->all())->save();
 
         if ($request->boolean('delete_consultant')) {
-            $curText = preg_replace('/(<div[^>]*>\s*?\((?:абзац введен|в ред\.|стать|введена|введен|пп\. .{3}+ в ред\.|п\. [0-9\.]+ в ред\.|п\. [0-9\.]+ введен|п|стать|пп|част|час) .*[0-9ФЗ]\))\s*?<\/div>/si', '', $curText);
+            $curText = preg_replace('/(<div[^>]*>\s*?\((?:абзац введен|в ред\.|введена|введен|пп\. .{3}+ в ред\.|п\. [0-9\.]+ в ред\.|п\. [0-9\.]+ введен|п) .+?[0-9]\))\s*?<\/div>/si', '', $curText);
             $curText = preg_replace('/\s?-\s?Федеральный закон от (?:[^\.]*\.){2}.*?[\.;]/si', "", $curText);;
-           // $curText = preg_replace('/(<div[^>]*>\s*?\(стать.*[0-9]\)\s*?<\/div>)/si', "", $curText);;
+            $curText = str_replace('margin-top:12.0pt;', "", $curText);
+            $curText = preg_replace('/(<div[^>]*>\s*?\((п|стать|пп|част).*[0-9ФЗ]\)\s*?<\/div>)/si', "", $curText);;
+            //$curText = preg_replace('/(<table[^>]*>[\s\S]*(Консу|Списо)[\s\S]*<\/table>)/si', "", $curText);;
         }
 
         if ($request->boolean('delete_a')) {
