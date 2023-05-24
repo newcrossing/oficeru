b45800746f6806a9f1bf130573e9807921dc91d0
newcrossing <newcrossing@gmail.com>
2023-05-24 18:35:23 +0300

    favicon

--- resources/views/front/moduls/favicon.blade.php
+++ resources/views/front/moduls/favicon.blade.php
@@ -5,6 +5,6 @@
 <link rel="icon" href="/favicon/favicon.svg" type="image/svg+xml">
 <link rel="icon" href="https://oficeru.ru/favicon.ico"  type="image/x-icon" sizes="any"><!-- 32x32 -->
 
-<link rel="manifest" href="manifest.webmanifest">
+<link rel="manifest" href="/manifest.webmanifest">
 <link rel="yandex-tableau-widget" href="/tableau.json">
 <meta content="https://oficeru.ru/assets/favicons/yandex.png" color=#fff" name="yandex-tableau-widget">
